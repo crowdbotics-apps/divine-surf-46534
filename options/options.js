c5a3b043ccb828df75b44a4fb2bae0aee1a7e4c5
Crowdbotics <team@crowdbotics.com>
2024-02-21 15:34:47 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "divine-surf-46534",
+  url: "https://divine-surf-46534.botics.co",
+  api: "https://divine-surf-46534.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
